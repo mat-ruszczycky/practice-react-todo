46a6c3d44b190c8e4afb16f95ee17485883e9d38
Mat Ruszczycky <mat_ruszczycky@apple.com>
2024-12-07 08:44:54 -0800

    chore: init of practive project

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(() => {
+    // Load from localStorage
+    const savedState = localStorage.getItem("myState");
+    return savedState ? JSON.parse(savedState) : 0;
+  });
+
+  useEffect(() => {
+    // Save to localStorage
+    localStorage.setItem("myState", JSON.stringify(count));
+  }, [count]);
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => setCount(0)}>
+          Clear Count
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
