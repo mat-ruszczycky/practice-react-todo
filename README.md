edd630179648c11e33334c7ec6cb5a27024e2469
Mat Ruszczycky <mat_ruszczycky@apple.com>
2024-12-07 11:20:13 -0800

    chore: fn requirements started

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# practice-react-todo
-Repo for react and sys design practice using TODO app
+# Practice React TODO App
+
+Repo for react and Front End system design practice using TODO app
+
+
+# Requirements
+
+## Functional
+
+### Core
+- Users can create items from the TODO list
+- Users can delete items from the TODO list
+- Users can mark item as complete
+
+
+### Additional
+- Users can update items from the TODO list
+- Users can filter tasks via category (completed, priority, date, alpha, etc...)
+- Users can set priority on items
+- Users can set deadlines
+
+
+## Non-Functional
+- Users can login and view a personal TODO list
+
+
+## Constraints
+- Must store tasks so they persist between sessions (e.g., using localStorage, a database, or cloud syncing)
+
+
+# Architecture Design
+
+## UI Mockup
+
+## High Level Design / Component Tree
+
+## Sequence Diagram / Userflows
+
+
+# Data Model 
+
+
+# Interface/API Model
+
+
+# Optimizations/Performance
--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,20 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState, useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(() => {
-    // Load from localStorage
-    const savedState = localStorage.getItem("myState");
-    return savedState ? JSON.parse(savedState) : 0;
-  });
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    // Save to localStorage
-    localStorage.setItem("myState", JSON.stringify(count));
-  }, [count]);
+
+  }, []);
 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={() => setCount(0)}>
-          Clear Count
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <header>
+      <h1>Hello world</h1>
+      <h2>Subheader</h2>
+     </header>
   )
 }
 
